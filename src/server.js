8cf40f74138652143e688c465fc56e0d481584b6
Evandro Francisco <evandrofbl@gmail.com>
2023-02-08 18:40:12 -0300

    Middleware to handle streams and json

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import http from 'node:http'
+import { json } from './middleware/json.js'
 
-
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
+    await json(req, res)
     return res.end('working!')
 })
 
